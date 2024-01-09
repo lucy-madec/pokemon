d522b918638d7b77614af75d86b1488aa80b227b
ines-lorquet <ines.lorquet@laplateforme.io>
2024-01-09 11:26:26 +0100

    class add_pokemon avec la méthode json qui contient les pokemon a rajouter

--- pokemon.py
+++ pokemon.py
@@ -2,16 +2,16 @@ import json
 class add_pokemon:
     def json(self):
         pokemon =[
-            {"nom": "Floravol", "type": "Plante", "niveau": 1, "Puissance": 45, "PV": 55, "Defense": 50},
-            {"nom": "Psykokwak", "type": "Plante", "niveau": 1, "Puissance": 52, "PV": 50, "Defense": 48},
-            {"nom": "Rondoudou", "type": "Fée", "niveau": 1, "Puissance": 30, "PV": 90, "Defense": 15},
-            {"nom": "Luxio", "type": "Electrique", "niveau": 1, "Puissance": 85, "PV": 60, "Defense": 49},
-            {"nom": "Etourvol", "type": "Vol", "niveau": 1, "Puissance": 75, "PV": 55, "Defense": 50},
-            {"nom": "Lainergie", "type": "Electrique", "niveau": 1, "Puissance": 55, "PV": 70, "Defense": 55},
-            {"nom": "Phanpy", "type": "Sol", "niveau": 1, "Puissance": 60, "PV": 90, "Defense": 60},
-            {"nom": "Magicarpe", "type": "Eau", "niveau": 1, "Puissance": 10, "PV": 20, "Defense": 55},
-            {"nom": "Gloupti", "type": "Poison", "niveau": 1, "Puissance": 43, "PV": 70, "Defense": 53},
-            {"nom": "Baudrive", "type": "Spectre", "niveau": 1, "Puissance": 50, "PV": 90, "Defense": 34}]
+            {"Nom": "Floravol", "Type": "Plante", "Niveau": 1, "Puissance": 45, "PV": 55, "Defense": 50},
+            {"Nom": "Psykokwak", "Type": "Plante", "Niveau": 1, "Puissance": 52, "PV": 50, "Defense": 48},
+            {"Nom": "Rondoudou", "Type": "Fée", "Niveau": 1, "Puissance": 30, "PV": 90, "Defense": 15},
+            {"Nom": "Luxio", "Type": "Electrique", "Niveau": 1, "Puissance": 85, "PV": 60, "Defense": 49},
+            {"Nom": "Etourvol", "Type": "Vol", "Niveau": 1, "Puissance": 75, "PV": 55, "Defense": 50},
+            {"Nom": "Lainergie", "Type": "Electrique", "Niveau": 1, "Puissance": 55, "PV": 70, "Defense": 55},
+            {"Nom": "Phanpy", "Type": "Sol", "Niveau": 1, "Puissance": 60, "PV": 90, "Defense": 60},
+            {"Nom": "Magicarpe", "Type": "Eau", "Niveau": 1, "Puissance": 10, "PV": 20, "Defense": 55},
+            {"Nom": "Gloupti", "Type": "Poison", "Niveau": 1, "Puissance": 43, "PV": 70, "Defense": 53},
+            {"Nom": "Baudrive", "Type": "Spectre", "Niveau": 1, "Puissance": 50, "PV": 90, "Defense": 34}]
         print(pokemon)
 pok = add_pokemon()
 print(pok.json())
