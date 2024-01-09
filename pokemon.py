c121212365a7e88483c2a879080437e68db77558
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-09 13:27:24 +0100

    début du code sur le fichier Pokemon

---
+++ pokemon.py
@@ -0,0 +1,11 @@
+class Pokemon:
+    def __init__(self, name, type, hp, level, attack_power, defense):
+        self.name = name
+        self.type = type
+        self.hp = hp
+        self.level = level
+        self.attack_power = attack_power
+        self.defense = defense
+
+    def display_pokemon(self):
+        return
