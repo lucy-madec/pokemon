dc24c99d419e0b095d6e50392341fcfb4b43e481
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-10 21:49:00 +0100

    ajout bouton "QUIT"

--- pokemon.py
+++ pokemon.py
@@ -1,11 +1,12 @@
 from global_def import Global
-import pygame,sys
+import pygame
+import sys
 
 class Pokemon(Global):
 
     def __init__(self):
         Global.__init__(self)
-    
+
     def background(self):
         self.img_back("Background", "images/images-partie/pokedex105.jpg")
 
@@ -13,13 +14,27 @@ class Pokemon(Global):
         # Récupérer la largeur de l'écran
         screen_width = self.screen.get_width()
 
-        # Dimensions du rectangle
-        rect_width = 200
-        rect_height = 100
+        # Dimensions du rectangle (plus petit et allongé en largeur)
+        rect_width = 40
+        rect_height = 20
 
         # Coordonnées du rectangle
-        rect_x = screen_width - rect_width - 50
-        rect_y = 50
+        rect_x = screen_width - rect_width - 10
+        rect_y = 10
+
+        # Récupérer les coordonnées de la souris
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        # Vérifier si la souris est sur le rectangle
+        if rect_x < mouse_x < rect_x + rect_width and rect_y < mouse_y < rect_y + rect_height:
+            # Agrandir le rectangle symétriquement en fonction de la position de la souris
+            enlargement_factor = 1.5  # Facteur d'agrandissement
+            rect_width = int(rect_width * enlargement_factor)
+            rect_height = int(rect_height / enlargement_factor)
+
+            # Ajuster les coordonnées pour rester dans le cadre du programme
+            rect_x = screen_width - rect_width - 10
+            rect_y = 10
 
         # Afficher le rectangle
         self.rect_radius(10, self.white, rect_x, rect_y, rect_width, rect_height)
@@ -28,7 +43,15 @@ class Pokemon(Global):
         text_x = rect_x + (rect_width - self.police_c1.size("QUIT")[0]) // 2
         text_y = rect_y + (rect_height - self.police_c1.size("QUIT")[1]) // 2
         self.text_c1("QUIT", self.black, text_x, text_y)
+
+        # Vérifier si un bouton de la souris est enfoncé
+        mouse_click = pygame.mouse.get_pressed()
+        if mouse_click[0] == 1:  # Clic gauche
+            pygame.quit()
+            sys.exit()
+
     
+
     def run(self):
         self.running = True
         while self.running:
