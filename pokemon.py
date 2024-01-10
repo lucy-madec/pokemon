c5495df571f89a57cabe96e409db7c6fd6f1534d
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-10 15:34:50 +0100

    modification fichier Pokemon

--- pokemon.py
+++ pokemon.py
@@ -6,36 +6,28 @@ class Pokemon(Global):
     def __init__(self):
         Global.__init__(self)
     
-    # Faire le Background
     def background(self):
         self.img_back("Background", "images/images-partie/pokedex105.jpg")
 
-        rect_width, rect_height = 200, 50
-        rect_x = self.screen_width - rect_width - 10
-        rect_y = 10
-        
-        # Dessiner le rectangle avec les coins arrondis
-        pygame.draw.rect(self.screen, self.white, (rect_x, rect_y, rect_width, rect_height), border_radius=10)
+    def button_quit(self):
+        # Récupérer la largeur de l'écran
+        screen_width = self.screen.get_width()
 
-        # Ajouter du texte centré dans le rectangle
-        font = pygame.font.Font(None, 30)
-        text = font.render("QUIT", True, self.black)
-        text_rect = text.get_rect(center=(rect_x + rect_width //2, rect_height //2))
-        self.screen.blit(text, text_rect)
+        # Dimensions du rectangle
+        rect_width = 200
+        rect_height = 100
 
-        # Récupérer de la position de la souris
-        mouse_x, mouse_y, = pygame.mouse.get_pos()
+        # Coordonnées du rectangle
+        rect_x = screen_width - rect_width - 50
+        rect_y = 50
 
-        # Vérifier si le clic de la souris est à l'intérieur du rectangle
-        if rect_x < mouse_x < rect_x + rect_width and rect_y < mouse_y < rect_y + rect_height:
-            pygame.draw.rect(self.screen, (200, 200, 200), (rect_x, rect_y, rect_width, rect_height), border_radius=10)
+        # Afficher le rectangle
+        self.rect_radius(10, self.white, rect_x, rect_y, rect_width, rect_height)
 
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    # Si le bouton gauche de la souris est cliqué, quitter le programme
-                    if event.button == 1:
-                        pygame.quit()
-                        sys.exit()
+        # Afficher le texte "QUIT" au centre du rectangle
+        text_x = rect_x + (rect_width - self.police_c1.size("QUIT")[0]) // 2
+        text_y = rect_y + (rect_height - self.police_c1.size("QUIT")[1]) // 2
+        self.text_c1("QUIT", self.black, text_x, text_y)
     
     def run(self):
         self.running = True
@@ -44,10 +36,10 @@ class Pokemon(Global):
                 if event.type == pygame.QUIT:
                     self.running = False
             self.background()
+            self.button_quit()
             pygame.display.flip()
             self.clock.tick(30)
         pygame.quit()
 
-if __name__ == "__main__":
-    game = Pokemon()
-    game.run()
+game = Pokemon()
+game.run()
