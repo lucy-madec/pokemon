06e87148598d0b7ee655a3615f175852aeebcbf2
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-11 15:24:57 +0100

    modifier images

--- ajout.py
+++ ajout.py
@@ -8,13 +8,13 @@ class Pokedex(Global):
         self.pikachu = Pikachu()
 
     def background(self):
-        background = pygame.image.load('images\images-pokedex\pokedex1020.png')
+        background = pygame.image.load('images/images-play/add_pokemon1.jpg')
         background = background.convert()
         self.screen.blit(background, (0,0))
 
     def pokemon(self):
         self.rect_radius(10,self.white,200, 40, 440, 80)
-        self.text_c3("POKEDEX",self.black,230,30)
+        self.text_c1("AJOUTER VOTRE POKÉMON",self.black,230,30)
 
         #Créer rectangles haut
         self.rect_radius(10,self.white,20, 250, 170, 120)
@@ -29,35 +29,35 @@ class Pokedex(Global):
         self.rect_radius(10,self.white,620, 450, 170, 120)
         
         #Afficher pokemon pikachu
-        self.img_pokemon("pikachu",'images\images-pokedex\pokedex1.png',110,119,45,245)
+        self.img_pokemon("pikachu",'images/images-play/Etourvol.png',110,119,45,245)
         self.text_c2("pikachu",self.black,60,345)
 
         #Afficher pokemon posipi
-        self.img_pokemon("posipi",'images\images-pokedex\pokedex3.png',115,119,45,440)
+        self.img_pokemon("posipi",'images/images-play/Lainergie.png',115,119,45,440)
         self.text_c2("posipi",self.black,70,545)
 
         #Afficher pokemon pyroli
-        self.img_pokemon("pyroli",'images\images-pokedex\pokedex4.png',120,129,245,242)
+        self.img_pokemon("pyroli",'images/images-play/Floravol.png',120,129,245,242)
         self.text_c2("pyroli",self.black,270,345)
 
         #Afficher pokemon noctali
-        self.img_pokemon("noctali",'images\images-pokedex\pokedex6.png',150,159,425,422)
+        self.img_pokemon("noctali",'images/images-play/Etourvol.png',150,159,425,422)
         self.text_c2("noctali",self.black,465,548)
 
         #Afficher pokemon salamèche
-        self.img_pokemon("salamèche",'images\images-pokedex\pokedex5.png',120,129,255,422)
+        self.img_pokemon("salamèche",'images/images-play/Magicarpe.png',120,129,255,422)
         self.text_c2("salamèche",self.black,245,545)
         
         #Afficher pokemon medhyena
-        self.img_pokemon("medhyena",'images\images-pokedex\pokedex7.png',290,299,570,300)
+        self.img_pokemon("medhyena",'images/images-play/Phanpy.png',290,299,570,300)
         self.text_c2("medhyena",self.black,655,545)
         
         #Afficher pokemon dracaufeu
-        self.img_pokemon("dracaufeu",'images\images-pokedex\pokedex8.png',130,139,455,223)
+        self.img_pokemon("dracaufeu",'images/images-play/Psykokwak.png',130,139,455,223)
         self.text_c2("dracaufeu",self.black,445,347)
         
         #Afficher pokemon caninos
-        self.img_pokemon("caninos",'images\images-pokedex\pokedex9.png',110,119,640,235)
+        self.img_pokemon("caninos",'images/images-play/Rondoudou.png',10,119,640,235)
         self.text_c2("caninos",self.black,655,347)
         
         #boutton changer de page
@@ -138,4 +138,7 @@ class Pokedex(Global):
             pygame.display.flip()
             self.clock.tick(30)
         pygame.quit()
+
+ajout = Pokedex()
+ajout.pokedex_run()
 
