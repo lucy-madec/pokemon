b2b731a2e71e07c9508ee658ac190d51cb02e8a1
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-11 09:19:51 +0100

    Modification fichier Pokemon

--- pokemon.py
+++ pokemon.py
@@ -40,8 +40,8 @@ class Pokemon(Global):
         self.rect_radius(10, self.white, rect_x, rect_y, rect_width, rect_height)
 
         # Afficher le texte "QUIT" au centre du rectangle
-        text_x = rect_x + (rect_width - self.police_c1.size("QUIT")[0]) // 2
-        text_y = rect_y + (rect_height - self.police_c1.size("QUIT")[1]) // 2
+        text_x = rect_x + (rect_width - self.police_c4.size("QUIT")[0]) // 2
+        text_y = rect_y + (rect_height - self.police_c4.size("QUIT")[1]) // 2
         self.text_c1("QUIT", self.black, text_x, text_y)
 
         # Vérifier si un bouton de la souris est enfoncé
@@ -49,46 +49,6 @@ class Pokemon(Global):
         if mouse_click[0] == 1:  # Clic gauche
             pygame.quit()
             sys.exit()
-
-    def button_back(self):
-        # Récupérer la largeur de l'écran
-        screen_width = self.screen.get_width()
-
-        # Dimensions du rectangle
-        rect_width = 40
-        rect_height = 20
-
-        # Coordonnées du rectangle
-        rect_x = 10
-        rect_y = 10
-
-        # Récupérer les coordonnées de la souris
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-
-        # Vérifier si la souris est sur le rectangle
-        if rect_x < mouse_x < rect_x + rect_width and rect_y < mouse_y < rect_y + rect_height:
-            # Agrandir le rectangle symétriquement en fonction de la position de la souris
-            enlargement_factor = 1.5  # Facteur d'agrandissement
-            rect_width = int(rect_width * enlargement_factor)
-            rect_height = int(rect_height / enlargement_factor)
-
-        # Ajuster les coordonnées pour rester dans le cadre du programme
-        rect_x = min(10, screen_width - rect_width - 10)
-        rect_y = 10
-
-        # Afficher le rectangle
-        self.rect_radius(10, self.white, rect_x, rect_y, rect_width, rect_height)
-
-        # Afficher le texte "BACK" au centre du rectangle
-        text_x = rect_x + (rect_width - self.police_c1.size("BACK")[0]) // 2
-        text_y = rect_y + (rect_height - self.police_c1.size("BACK")[1]) // 2
-        self.text_c1("BACK", self.black, text_x, text_y)
-
-        # Vérifier si un bouton de la souris est enfoncé
-        mouse_click = pygame.mouse.get_pressed()
-        if mouse_click[0] == 1:  # Clic gauche
-            # Ajoutez ici le code pour effectuer l'action "BACK"
-            print("Button BACK clicked")     
 
     def run(self):
         self.running = True
