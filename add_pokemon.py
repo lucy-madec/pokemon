93c7ba26f5d244ea1eb0421f69ba78c4f797510c
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-11 21:14:43 +0100

    modifier présentation

--- add_pokemon.py
+++ add_pokemon.py
@@ -13,16 +13,7 @@ class Pokedex(Global):
         self.screen.blit(background, (0,0))
 
     def logo(self):
-        self.img_pokemon("Etourvol",'images/images-add/add_pokemon1.png',70,89,75,255)
-
-        def img_pokemon(self,name,path,a,b,x,y,):
-        name = pygame.image.load(path)
-        name = name.convert_alpha()
-        name = pygame.transform.scale(name,(a,b))        
-        self.screen.blit(name,(x,y))
-
-     
-
+        self.img_pokemon("tagline",'images/images-add/add_pokemon9.png',175,100,340,110)     
 
     def pokemon(self):
         
@@ -46,32 +37,32 @@ class Pokedex(Global):
         self.text_c2("Etourvol",self.black,60,342)
 
         #Afficher pokemon Lainergie
-        self.img_pokemon("Lainergie",'images/images-add/add_pokemon3.png',85,89,45,430)
-        self.text_c2("Lainergie",self.black,70,542)
+        self.img_pokemon("Lainergie",'images/images-add/add_pokemon3.png',85,89,65,455)
+        self.text_c2("Lainergie",self.black,50,542)
 
         #Afficher pokemon Floravol
-        self.img_pokemon("Floravol",'images/images-add/add_pokemon2.png',120,129,245,242)
-        self.text_c2("Floravol",self.black,270,342)
+        self.img_pokemon("Floravol",'images/images-add/add_pokemon2.png',100,119,265,242)
+        self.text_c2("Floravol",self.black,265,342)
 
         #Afficher pokemon Luxio
-        self.img_pokemon("Luxio",'images/images-add/add_pokemon4.png',150,159,425,422)
-        self.text_c2("Luxio",self.black,465,542)
+        self.img_pokemon("Luxio",'images/images-add/add_pokemon4.png',90,109,450,445)
+        self.text_c2("Luxio",self.black,470,542)
 
         #Afficher pokemon Magicarpe
-        self.img_pokemon("Magicarpe",'images/images-add/add_pokemon5.png',120,129,255,422)
+        self.img_pokemon("Magicarpe",'images/images-add/add_pokemon5.png',90,99,255,452)
         self.text_c2("Magicarpe",self.black,245,542)
         
         #Afficher pokemon Phanpy
         self.img_pokemon("Phanpy",'images/images-add/add_pokemon6.png',80,99,655,450)
-        self.text_c2("Phanpy",self.black,655,542)
+        self.text_c2("Phanpy",self.black,670,542)
         
         #Afficher pokemon Psykokwak
-        self.img_pokemon("Psykokwak",'images/images-add/add_pokemon7.png',130,139,455,223)
-        self.text_c2("Psykokwak",self.black,445,342)
+        self.img_pokemon("Psykokwak",'images/images-add/add_pokemon7.png',70,89,465,253)
+        self.text_c2("Psykokwak",self.black,440,342)
         
         #Afficher pokemon Rondoudou
-        self.img_pokemon("Rondoudou",'images/images-add/add_pokemon8.png',110,119,640,235)
-        self.text_c2("Rondoudou",self.black,655,342)       
+        self.img_pokemon("Rondoudou",'images/images-add/add_pokemon8.png',70,79,670,258)
+        self.text_c2("Rondoudou",self.black,642,342)       
         
         pygame.display.update()
         pygame.display.flip()
@@ -142,7 +133,8 @@ class Pokedex(Global):
                     if button_rect.collidepoint(mouse_x, mouse_y):
                         running = False     
                                                                                     
-            self.background()            
+            self.background()  
+            self.logo()          
             self.pokemon()
             pygame.display.flip()
             self.clock.tick(30)
