30816f8d4f4c1fd30dc7d6e975f3e389132b5817
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-14 16:57:35 +0100

    Ajouter options de combat avec effet hoover et créer une section texte

--- play_fight.py
+++ play_fight.py
@@ -7,15 +7,28 @@ class Play_Fight(Global):
     def __init__(self):
         # Appelle le constructeur de la classe parent Global
         Global.__init__(self)
-        # Charge l'image pour l'attaque
-        self.attack_image = pygame.image.load("images/images-play/play2.png")
-        # Charge l'image pour la défense
-        self.offensive_image = pygame.image.load("images/images-play/play3.png")
-
+    
     def background(self):
         # Affiche l'image de fond
         self.img_back("Background", "images/images-play/play1.jpg")
 
+    def rectangle(self): 
+
+        # Rectangle blanc texte
+        self.rect_radius(10,self.white,55,430,240,115)  
+       
+
+        # Rectangle texte
+        self.img_pokemon("rectangle_texte",'images/images-play/play5.png', 250,129,50,422)    
+
+        self.text_c2("What will you do ? ", self.black, 70, 475)       
+
+        # Rectangle 4actions
+
+        self.rect_radius(10,self.white,335,430,430,115)  
+        
+        self.img_pokemon("rectangle_option",'images/images-play/play5.png',445,129,325,422)        
+       
     def button_quit(self):
         # Affiche le bouton QUIT
         self.rect_radius(5, self.white, 720, 10, 70, 25)
@@ -29,20 +42,37 @@ class Play_Fight(Global):
     def is_mouse_over_button(self, button_rect):
         # Vérifie si la souris est au-dessus du bouton
         mouse_pos = pygame.mouse.get_pos()
-        return button_rect.collidepoint(mouse_pos)
+        return button_rect.collidepoint(mouse_pos)    
     
-    def attack_button(self):
+    def fight_button(self):
         # Affiche le bouton d'attaque
-        self.rect_radius(5, self.orange, 550, 450, 95, 70)
-        self.text_c1("ATTACK", self.black, 563, 449)
-        self.screen.blit(self.attack_image, (575, 465))
+        self.rect_radius(5, self.pink, 350, 450, 95, 75)
+        self.text_c1("FIGHT", self.black, 370, 475)
+    
+    def bag_button(self):
+        self.rect_radius(5, self.brown, 450, 450, 95, 75)
+        self.text_c1("BAG", self.white, 480, 475)     
 
-    def offensive_button(self):
+    def pokemon_button(self):
         # Affiche le bouton de défense
-        self.rect_radius(5, self.blue, 660, 450, 95, 70)
-        self.text_c4("OFFENSIVE", self.white, 613, 449)
-        self.screen.blit(self.offensive_image, (575, 465))
+        self.rect_radius(5, self.green, 550, 450, 95, 75)
+        self.text_c1("POKEMON", self.white, 555, 475)
 
+    def run_button(self):
+        self.rect_radius(5, self.blue, 650, 450, 95, 75)
+        self.text_c1("RUN", self.white, 680, 475)      
+
+    def draw_hover_rectangle(self, btn_rect): 
+        # Afficher le rectangle noir au survol de la souris
+        if self.is_mouse_over_button(btn_rect):
+            pygame.draw.rect(self.screen, self.black, btn_rect, 4, 5)
+    
+    def rect_hover(self):         
+        self.draw_hover_rectangle(pygame.Rect(450, 450, 95, 75))  # Bag
+        self.draw_hover_rectangle(pygame.Rect(350, 450, 95, 75))  # Fight
+        self.draw_hover_rectangle(pygame.Rect(550, 450, 95, 75))  # Pokemon
+        self.draw_hover_rectangle(pygame.Rect(650, 450, 95, 75))  # Run
+    
     def run(self):
         # La boucle principale du jeu
         self.running = True
@@ -61,10 +91,18 @@ class Play_Fight(Global):
 
             # Affiche les éléments à l'écran
             self.background()
+            self.rectangle()
             self.button_quit()
             self.button_back()
-            self.attack_button()
-            self.offensive_button()
+            self.fight_button()
+            self.pokemon_button()
+            self.bag_button()
+            self.run_button()
+
+            self.rect_hover()
+
+           
+            
             pygame.display.flip()
             self.clock.tick(30)
             
