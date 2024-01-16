2760df68e76475f526ed350dacf97cf8e38df77a
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-16 16:31:50 +0100

    Modifier la taille de la police

--- global_def.py
+++ global_def.py
@@ -21,7 +21,7 @@ class Global:
         self.police_c2 = pygame.font.Font("Pokemon Classic.ttf",15)  
         self.police_c3 = pygame.font.Font("Pokemon Classic.ttf",50)
         self.police_c4 = pygame.font.Font("Pokemon Classic.ttf",5)  
-        self.police_c5 = pygame.font.Font("Pokemon Classic.ttf",35)    
+        self.police_c5 = pygame.font.Font("Pokemon Classic.ttf",15)    
         self.police_c6 = pygame.font.Font("Pokemon Classic.ttf", 12)    
         self.police_p1 = pygame.font.Font("Pixeled.ttf", 16)
         self.lst_name = []
