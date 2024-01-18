0a238c755ae87f10618c94c18af42d379a9c5f08
ines-lorquet <ines.lorquet@laplateforme.io>
2024-01-18 13:33:15 +0100

    modification design de add_pokemon

--- add_pokemon.py
+++ add_pokemon.py
@@ -14,8 +14,8 @@ class Add_Pokemon(Global):
         self.screen.blit(background, (0,0))
 
     # Afficher le logo "Catch Them All"
-    def logo(self):
-        self.img_pokemon("tagline",'images/images-add/add_pokemon9.png',175,100,340,110) 
+    # def logo(self):
+    #     self.img_pokemon("tagline",'images/images-add/add_pokemon9.png',175,100,340,110) 
     
     # Afficher le bouton QUIT
     def button_quit(self):        
@@ -42,8 +42,8 @@ class Add_Pokemon(Global):
     #     back_menu_rect = pygame.Rect(540, 10, 70, 25)
     #     return back_menu_rect.collidepoint(mouse_pos)
     
-    def logo(self):
-        self.img_pokemon("tagline",'images/images-add/add_pokemon9.png',175,100,340,110)     
+    # def logo(self):
+    #     self.img_pokemon("tagline",'images/images-add/add_pokemon9.png',175,100,340,110)     
 
     #  Afficher liste des 8 Pokémons
     def pokemon(self):
@@ -53,48 +53,48 @@ class Add_Pokemon(Global):
         self.text_c5("ADD POKEMON",self.black,350,65)
         
         # Créer rectangles haut
-        self.rect_radius(10,self.white,20, 250, 470, 120)
-        # self.rect_radius(10,self.white,220, 250, 170, 120)
-        self.rect_radius(10,self.white,420, 250, 470, 120)
-        self.rect_radius(10,self.white,620, 250, 170, 120)
+        self.rect_radius(10,self.white,100, 490, 600, 100)
+        self.rect_radius(10,self.white,100, 130, 600, 100)
+        self.rect_radius(10,self.white,100, 250, 600, 100)
+        self.rect_radius(10,self.white,100, 370, 600, 100)
     
         # Créer rectangles bas
-        self.rect_radius(10,self.white,20, 450, 470, 120)
+        # self.rect_radius(10,self.white,20, 450, 470, 120)
         # self.rect_radius(10,self.white,220, 450, 170, 120)
-        self.rect_radius(10,self.white,420, 450, 470, 120)
+        # self.rect_radius(10,self.white,420, 450, 470, 120)
         # self.rect_radius(10,self.white,620, 450, 170, 120)
         
-        # Afficher pokemon Etourvol
-        self.img_pokemon("Etourvol",'images/images-add/add_pokemon1.png',70,89,75,255)
-        self.text_c2("Etourvol",self.black,60,342)
+        # # Afficher pokemon Etourvol
+        # self.img_pokemon("Etourvol",'images/images-add/add_pokemon1.png',70,89,75,255)
+        # self.text_c2("Etourvol",self.black,60,342)
 
-        # Afficher pokemon Lainergie
-        self.img_pokemon("Lainergie",'images/images-add/add_pokemon3.png',85,89,65,455)
-        self.text_c2("Lainergie",self.black,50,542)
+        # # Afficher pokemon Lainergie
+        # self.img_pokemon("Lainergie",'images/images-add/add_pokemon3.png',85,89,65,455)
+        # self.text_c2("Lainergie",self.black,50,542)
 
-        # Afficher pokemon Floravol
-        self.img_pokemon("Floravol",'images/images-add/add_pokemon2.png',100,119,265,242)
-        self.text_c2("Floravol",self.black,265,342)
+        # # Afficher pokemon Floravol
+        # self.img_pokemon("Floravol",'images/images-add/add_pokemon2.png',100,119,265,242)
+        # self.text_c2("Floravol",self.black,265,342)
 
-        # Afficher pokemon Luxio
-        self.img_pokemon("Luxio",'images/images-add/add_pokemon4.png',90,109,450,445)
-        self.text_c2("Luxio",self.black,470,542)
+        # # Afficher pokemon Luxio
+        # self.img_pokemon("Luxio",'images/images-add/add_pokemon4.png',90,109,450,445)
+        # self.text_c2("Luxio",self.black,470,542)
 
-        # Afficher pokemon Magicarpe
-        self.img_pokemon("Magicarpe",'images/images-add/add_pokemon5.png',90,99,255,452)
-        self.text_c2("Magicarpe",self.black,245,542)
+        # # Afficher pokemon Magicarpe
+        # self.img_pokemon("Magicarpe",'images/images-add/add_pokemon5.png',90,99,255,452)
+        # self.text_c2("Magicarpe",self.black,245,542)
         
-        # Afficher pokemon Phanpy
-        self.img_pokemon("Phanpy",'images/images-add/add_pokemon6.png',80,99,655,450)
-        self.text_c2("Phanpy",self.black,670,542)
+        # # Afficher pokemon Phanpy
+        # self.img_pokemon("Phanpy",'images/images-add/add_pokemon6.png',80,99,655,450)
+        # self.text_c2("Phanpy",self.black,670,542)
         
-        # Afficher pokemon Psykokwak
-        self.img_pokemon("Psykokwak",'images/images-add/add_pokemon7.png',70,89,465,253)
-        self.text_c2("Psykokwak",self.black,440,342)
+        # # Afficher pokemon Psykokwak
+        # self.img_pokemon("Psykokwak",'images/images-add/add_pokemon7.png',70,89,465,253)
+        # self.text_c2("Psykokwak",self.black,440,342)
         
-        # Afficher pokemon Rondoudou
-        self.img_pokemon("Rondoudou",'images/images-add/add_pokemon8.png',70,79,670,258)
-        self.text_c2("Rondoudou",self.black,642,342) 
+        # # Afficher pokemon Rondoudou
+        # self.img_pokemon("Rondoudou",'images/images-add/add_pokemon8.png',70,79,670,258)
+        # self.text_c2("Rondoudou",self.black,642,342) 
 
     # def read_json(self,name):
     #     with open('add_json.json', 'r') as json_file:
@@ -121,7 +121,7 @@ class Add_Pokemon(Global):
 
     def setup_screen(self):
         self.background()
-        self.logo()
+        # self.logo()
         self.pokemon()
         self.button_quit()
 
